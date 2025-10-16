1ffa301ec6f5188eab16a4158bd8566247839c8a
cbnuLeehyunwoo <henow123@naver.com>
2025-10-17 03:07:09 +0900

    feat(Application.kt): 커스텀 구분자까지 고려하여 숫자를 추출하는 기능 구현
    - extractNumList()함수에 구분자 리스트를 만들어 커스텀 구분자가 식별되면 해당 리스트에 추가하는 로직을 추가했습니다
    - 기존 코드의 재사용을 위해 커스텀 구분자가 존재한다면 일반 입력과
      비슷한 형식으로(\n 이후의 서브스트링) 변환해주는 getNumberSection()함수를 추가했습니다

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,23 +6,30 @@ fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val inputString = readUserString()
     val cumstomDlmt = extractCustomDelimiter(inputString)
-    val numList = extractNumList(inputString)
+    val numberSection = getNumberSection(inputString, customDlmt)
+    val numList = extractNumList(numberSection, customDlmt)
     val sum = sumNumList(numList)
     println("결과: $sum")
 }
 
+fun getNumberSection( input: String, customDlmt: String?): String {
+    if(customDlmt != null) return input.substringAfter("\n")
+    return input
+}
+
 fun sumNumList(numList: List<Int>) = numList.sumOf{it}
 
-fun extractNumList(input: String): List<Int> {
+fun extractNumList(input: String, customDlmt: String?): List<Int> {
     if(input.isEmpty()) return listOf(0)
-    val strNumList = input.split(",", ":")
+    val delimeters = mutableListOf(",", ":")
+    if (customDlmt != null) delimeters += customDlmt
+    val strNumList = input.split(*delimeters.toTypedArray())
     val intNumList = mutableListOf<Int>()
     for(strNum in strNumList) {
         intNumList += strNum.toInt()
     }
     return intNumList
 }
-
 fun extractCustomDelimiter(input: String) :String?{
     val customDlmtRegex = Regex("^//(.*?)\\n")
     val match = customDlmtRegex.find(input)
