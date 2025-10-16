a73e66dd0d018f448bc80dd3290afb304374e0e9
cbnuLeehyunwoo <henow123@naver.com>
2025-10-16 22:03:38 +0900

    feat(Application.kt): 사용자가 입력한 문자열에서 기본 구분자를 기준으로 숫자 리스트를 추출하는 기능 구현
    - 문자열을 입력받아 구분자를 기준으로 문자를 나누고 Int형으로 변환해 Int형 리스트를 반환하는 extractNumList() 함수를 추가했습니다.
    - 사용자가 아무 입력도 하지 않고 입력을 종료하면 0이 담긴 리스트를 리턴하도록 하였습니다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,9 +3,18 @@ package calculator
 import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
-    // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
     val inputString = readUserString()
+    val numList = extractNumList(inputString)
+}
+fun extractNumList(input: String): List<Int> {
+    if(input.isEmpty()) return listOf(0)
+    val strNumList = input.split(",", ":")
+    val intNumList = mutableListOf<Int>()
+    for(strNum in strNumList) {
+        intNumList += strNum.toInt()
+    }
+    return intNumList
 }
 
 fun readUserString(): String {
