0cf8e8607de1bc16a51a5cc666e47c9969ed2e15
cbnuLeehyunwoo <henow123@naver.com>
2025-10-16 23:31:00 +0900

    feat(Application.kt): 커스텀 구분자를 지정하는 기능 구현
    - 문자열을 입력받아 커스텀 구분자를 문자열 형태로 반환하는 extractCustomDelimiter() 함수를 추가했습니다.
    - 정규 표현식을 이용하여 패턴을 매칭하고(?를 통한 lazy matching), 매칭된 패턴을 그룹화하여 1번째 인덱스에 속한 그룹(커스텀 구분자)을 반환합니다
    - 패턴이 매칭되지 않으면(커스텀 구분자를 지정하지 않았다면) Null 값이 반환됩니다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console.readLine
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val inputString = readUserString()
+    val cumstomDlmt = extractCustomDelimiter(inputString)
     val numList = extractNumList(inputString)
     val sum = sumNumList(numList)
     println("결과: $sum")
@@ -20,6 +21,13 @@ fun extractNumList(input: String): List<Int> {
         intNumList += strNum.toInt()
     }
     return intNumList
+}
+
+fun extractCustomDelimiter(input: String) :String?{
+    val customDlmtRegex = Regex("^//(.*?)\\n")
+    val match = customDlmtRegex.find(input)
+    val customDlmt = match?.groupValues?.get(1)
+    return customDlmt
 }
 
 fun readUserString(): String {
